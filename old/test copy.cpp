68d5b765e1c5042f4d5b5031169441210e9b403c
ChaoticMagician <ainidetianshi21@163.com>
2021-02-05 15:37:19 +0800

    init

---
+++ old/test copy.cpp
@@ -0,0 +1,14 @@
+#include "./include/best_header.h"
+#include "./include/Sales_item.h"
+int main(){
+	// Sales_item item1,item2;
+	// std::cin >> item1 >> item2;
+	// std::cout << item1 + item2 << std::endl;
+	Sales_item item1;
+	std::cin >> item1;
+	std::cout << item1 << std::endl;
+
+	int a;
+	std::cin >> a;
+	return 0;
+}   
---
+++ test2.cpp
@@ -0,0 +1,48 @@
+#include "./include/best_header.h"
+#include "./include/Sales_item.h"
+	// int i= 100 ,sum = 0;
+int main(){
+	// Sales_item item1,item2;
+	// std::cin >> item1 >> item2;
+	// std::cout << item1 + item2 << std::endl;
+	// Sales_item item1,item2;
+	// std::cin >> item1 >> item2;
+	// if (item1.isbn()==item2.isbn()){
+	// 	std::cout<<item1+item2<<std::endl;
+	// 	return 0;
+	// }
+	// else{
+	// 	std::cout<<"Data must refer to same ISBN"<<std::endl;
+	// 	return -1;
+	// }
+	// std::cout << item1 << std::endl;
+	// for (int i = 0; i !=10; i++)
+	// {
+	// 	sum+=i;
+	// };
+	// std::cout<<sum<<std::endl;
+	
+
+	// //引用1
+	// int ival = 1.1;
+	// int &rvall = ival;
+	// int &refval2 = rvall;
+	// std::cout<<refval2<<std::endl;
+
+	//引用2
+	// int i=1.2,&refi = i;
+	// double d = 8.234,&refd = d;
+	// // refi=d;
+	// // refd = refi;
+	// d = 3.1415926535;
+	// i  = refd;
+	// refd = 1.23456789;
+	// std::cout<< std::setprecision(9) <<i<<",,"<<refi<<std::endl;
+	// std::cout<< std::setprecision(10) <<d<<",,"<<refd<<std::endl;
+	
+
+
+	int a;
+	std::cin >> a;
+	return 0;
+}
